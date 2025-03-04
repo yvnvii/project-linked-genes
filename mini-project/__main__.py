9fa12b5750e5d458c91378f412f68d5c8d39a7a6
yvnvii <yo2368@columbia.edu>
2025-03-03 22:52:48 -0500

    genes to phenotypes, phenotypes to genes methods

--- mini-project/__main__.py
+++ mini-project/__main__.py
@@ -5,57 +5,50 @@ Command line interface to darwin_day
 """
 
 import argparse
-from darwinday import birthday, info
-
+import emoji
+from module import Module
 
 def parse_command_line():
-    "parses args for the darwin_day funtion"
+    "parses args for the funtion"
 
     # init parser and add arguments
     parser = argparse.ArgumentParser()
 
     # add long args
     parser.add_argument(
-        "--next",
-        help="returns a countdown until the next Darwin Day",
+        "--g2p",
+        help="returns phenotypes from given genes",
         action="store_true")
 
     # add long args
     parser.add_argument(
-        "--last",
-        help="returns a countdown since the last Darwin Day",
-        action="store_true")
-
-    parser.add_argument(
-        "--info",
-        help="returns a random fact about Chuck D.",
+        "--p2g",
+        help="returns genes from given phenotypes",
         action="store_true")
 
     # parse args
-    args = parser.parse_args()
-
-    # check that user only entered one action arg
-    if sum([args.next, args.last, args.info]) > 1:
-        raise SystemExit(
-            "only one of 'next', 'last' or 'info' at a time.")
-    return args
-
+    return parser.parse_args()
 
 def main():
     "run main function on parsed args"
 
+    print(emoji.emojize(":rocket: Main program started"))
+
     # get arguments from command line as a dict-like object
     args = parse_command_line()
+    module_instance = Module()
 
-    # pass argument to call darwinday function
-    if args.next:
-        birthday('next')
-    elif args.last:
-        birthday('last')
-    elif args.info:
-        info()
+    # pass argument to call function
+    if args.g2p:
+        module_instance.get_phenotypes_call()
+        
+    elif args.p2g:
+        module_instance.get_genotypes_call()
 
+    else:
+        print(emoji.emojize(":warning: No valid option provided."))
+        print(emoji.emojize("Use --g2p (genes to phenotypes), or --p2g (phenotypes to genes)."))
 
 if __name__ == "__main__":
-    birthday('next')
-    info()
+    main()
+    
--- mini-project/module.py
+++ mini-project/module.py
@@ -1,42 +1,68 @@
 import requests
-
-ENSEMBL_API = "https://rest.ensembl.org"
-
-def get_phenotypes(gene_symbol, species):
-  """Fetches phenotypes associated with a gene from Ensembl API."""
-  url = f"{ENSEMBL_API}/phenotype/gene/{species}/{gene_symbol}?content-type=application/json"
-  response = requests.get(url)
-  if response.status_code == 200:
-    data = response.json()
-    print(data)
-  else:
-    print(f"Error fetching phenotypes for {gene_symbol}: {response.status_code}")
-    return []
-
-#example usage
-user_species = input("Enter species name: ")
-user_genes = input("Enter gene names: ")
-genelist = user_genes.split()
-for gene in genelist:
-  phenotypes = get_phenotypes(user_genes, user_species)
-  print(f"Phenotypes for {gene}: {phenotypes}")
+import json
+import emoji
 
 
-def get_genes(phenotype_term, species):
-  url = f"{ENSEMBL_API}/phenotype/term/{species}/{phenotype_term}?content-type=application/json"
-  response = requests.get(url)
-  if response.status_code == 200:
-    data = response.json()
-    genes = [entry["Gene"] for entry in data if "Gene" in entry]
-    genes = genes + [entry["Variation"] for entry in data if "Variation" in entry]
-    return genes
-  else:
-    print(f"Error fetching genes for {phenotype_term}: {response.status_code}")
-    return []
-'''
-#example
-user_species = input("Enter species name: ")
-user_phenotype = input("Enter phenotype term: ")
-genes = get_genes(user_phenotype, user_species)
-print(f"Genes for {user_phenotype}: {genes}")
-'''
+class Module:
+
+  ENSEMBL_API = "https://rest.ensembl.org"
+
+
+  @staticmethod
+  def get_phenotypes(gene_symbol, species):
+    """Fetches phenotypes associated with a gene from Ensembl API."""
+    url = f"{Module.ENSEMBL_API}/phenotype/gene/{species}/{gene_symbol}?content-type=application/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+      phenotypes = [entry["description"] for entry in data if "description"in entry]
+      return (phenotypes)
+    else:
+      print(emoji.emojize(":warning: "), f"Error fetching phenotypes for {gene_symbol}: {response.status_code}")
+      return []
+
+  @staticmethod
+  def get_genes(phenotype_term, species):
+    url = f"{Module.ENSEMBL_API}/phenotype/term/{species}/{phenotype_term}?content-type=application/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+      genes = [entry["Gene"] for entry in data if "Gene" in entry]
+      genes = genes + [entry["Variation"] for entry in data if "Variation" in entry]
+      return genes
+    else:
+      print(emoji.emojize(":warning: "),f"Error fetching genes for {phenotype_term}: {response.status_code}")
+      return []
+
+  def get_phenotypes_call(self):
+    #example usage
+    user_species = input(emoji.emojize(":magnifying_glass_tilted_right: Enter species name: "))
+    user_genes = input(emoji.emojize(":dna: Enter gene names: "))
+    genelist = user_genes.split()
+
+    for gene in genelist:
+      phenotypes = self.get_phenotypes(gene, user_species)
+      if phenotypes:
+        with open(f"{user_species}_{gene}.txt", "a") as file:
+          file.write(json.dumps(phenotypes, indent=2) + "\n")
+        print(emoji.emojize(":floppy_disk: "), f"phenotypes for {gene} saved to {user_species}_{gene}.txt")
+      else:
+        print(emoji.emojize(":cross_mark: "), f"No phenotypes found for {gene}")
+
+  def get_genotypes_call(self):
+    user_species = input(emoji.emojize(":magnifying_glass_tilted_right: Enter species name: "))
+    user_phenotypes = input(emoji.emojize(":microbe: Enter phenotype term: "))
+    phenotypelist = user_phenotypes.split()
+
+    for pheno in phenotypelist:
+      genes = self.get_genes(pheno, user_species)
+      if genes:
+        with open(f"{user_species}_{pheno}.txt", "a") as file:
+          file.write(json.dumps(genes, indent=2) + "\n")
+        print(emoji.emojize(":floppy_disk: "), f"genes for {pheno} saved to {user_species}_{pheno}.txt")
+      else:
+        print(emoji.emojize(":cross_mark: "),f"No phenotypes found for {pheno}")
+
+
+
+
