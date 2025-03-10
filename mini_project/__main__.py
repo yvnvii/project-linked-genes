9c59a36b80f4ce031dc07cc10d7e000b015e2cda
yvnvii <yo2368@columbia.edu>
2025-03-10 18:57:01 -0400

    added get_ld

---
+++ mini_project/__main__.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+Command line interface to darwin_day
+"""
+
+import argparse
+import emoji
+from mini_project.module import Module
+
+def parse_command_line():
+    "parses args for the funtion"
+
+    # init parser and add arguments
+    parser = argparse.ArgumentParser()
+
+    # add long args
+    parser.add_argument(
+        "--g2p",
+        help="returns phenotypes from given genes",
+        action="store_true")
+
+    # add long args
+    parser.add_argument(
+        "--p2g",
+        help="returns genes from given phenotypes",
+        action="store_true")
+
+    parser.add_argument(
+        "--g2ld",
+        help = "returns linked genes to given genes",
+        action = "store_true")
+
+    # parse args
+    return parser.parse_args()
+
+def main():
+    "run main function on parsed args"
+
+    print(emoji.emojize(":rocket: Main program started"))
+
+    # get arguments from command line as a dict-like object
+    args = parse_command_line()
+    module_instance = Module()
+
+    # pass argument to call function
+    if args.g2p:
+        module_instance.get_phenotypes_call()
+        
+    elif args.p2g:
+        module_instance.get_genotypes_call()
+    elif args.g2ld:
+        module_instance.get_ld_variants_call()
+
+    else:
+        print(emoji.emojize(":warning: No valid option provided."))
+        print(emoji.emojize("Use --g2p (genes to phenotypes), or --p2g (phenotypes to genes)."))
+
+if __name__ == "__main__":
+    main()
+    
---
+++ mini_project/module.py
@@ -0,0 +1,111 @@
+import requests
+import json
+import emoji
+import re
+
+class Module:
+
+  ENSEMBL_API = "https://rest.ensembl.org"
+
+  def get_phenotypes(self, gene_symbol, species):
+
+    self.gene = gene_symbol
+    self.species = species
+
+    """Fetches phenotypes associated with a gene from Ensembl API."""
+    url = f"{Module.ENSEMBL_API}/phenotype/gene/{species}/{gene_symbol}?content-type=application/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+      phenotypes = [entry["description"] for entry in data if "description"in entry]
+      return (phenotypes)
+    else:
+      print(emoji.emojize(":warning: "), f"Error fetching phenotypes for {gene_symbol}: {response.status_code}")
+      return []
+
+  def get_genes(self, phenotype_term, species):
+    self.phenotype = phenotype_term
+    self.species = species
+
+    url = f"{Module.ENSEMBL_API}/phenotype/term/{species}/{phenotype_term}?content-type=application/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+      genes = [entry["Gene"] for entry in data if "Gene" in entry]
+      genes = genes + [entry["Variation"] for entry in data if "Variation" in entry]
+      return genes
+    else:
+      print(emoji.emojize(":warning: "),f"Error fetching genes for {phenotype_term}: {response.status_code}")
+      return []
+
+  def get_ld_variants(self, species, variant_id, population):
+    """
+    Fetch LD variants for a specific variant ID.
+    """
+    url = f"{Module.ENSEMBL_API}/ld/{species}/{variant_id}/{population}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()  # Returns linked variants
+    return None
+
+  def clean_input(self, text):
+    """Remove surrounding quotes and spaces from input."""
+    return re.sub(r'^["\']|["\']$', '', text.strip())
+
+
+  def get_phenotypes_call(self):
+    #example usage
+    user_species = input(emoji.emojize(":magnifying_glass_tilted_right: Enter species name: "))
+    user_genes = input(emoji.emojize(":dna: Enter gene names: "))
+    genelist = user_genes.split()
+
+    for gene in genelist:
+      phenotypes = self.get_phenotypes(gene, user_species)
+      if phenotypes:
+        with open(f"{user_species}_{gene}.txt", "a") as file:
+          file.write(json.dumps(phenotypes, indent=2) + "\n")
+        print(emoji.emojize(":floppy_disk: "), f"phenotypes for {gene} saved to {user_species}_{gene}.txt")
+      else:
+        print(emoji.emojize(":cross_mark: "), f"No phenotypes found for {gene}")
+
+  def get_genotypes_call(self):
+    user_species = input(emoji.emojize(":magnifying_glass_tilted_right: Enter species name: "))
+    user_phenotypes = input(emoji.emojize(":microbe: Enter phenotype term: "))
+    phenotypelist = user_phenotypes.split()
+
+    for pheno in phenotypelist:
+      genes = self.get_genes(pheno, user_species)
+      if genes:
+        with open(f"{user_species}_{pheno}.txt", "a") as file:
+          cleaned_genes = [self.clean_input(gene) for gene in genes] 
+          file.write("\n".join(cleaned_genes) + "\n")
+        print(emoji.emojize(":floppy_disk: "), f"genes for {pheno} saved to {user_species}_{pheno}.txt")
+      else:
+        print(emoji.emojize(":cross_mark: "),f"No phenotypes found for {pheno}")
+
+    if(input("proceed to find linked genes? [y/n]: ")=="y"):
+      self.get_ld_variants_call(file = f"{user_species}_{pheno}")
+    else:
+      print("done")
+
+  def get_ld_variants_call(self, file= False):
+
+    filepath = file if file else input(emoji.emojize(":magnifying_glass_tilted_right: Enter file path with a list of genes: "))
+    species = species if file else input(emoji.emojize(":magnifying_glass_tilted_right: Enter species name: "))
+    population = input(emoji.emojize(":magnifying_glass_tilted_right: Enter population: "))
+
+
+    output = f"ld_{filepath}.txt"
+
+    try:
+      with open(f'{filepath}.txt', 'r') as file, open(output, "a") as output_file:
+        for gene in file:
+          gene = gene.strip()
+          variants = self.get_ld_variants(species, gene, population)
+          output_file.write(f"linked variants for {gene}:\n\n")
+          output_file.write(json.dumps(variants, indent=2)+"\n")
+      print(emoji.emojize(":floppy_disk: "), f"genes linked to {filepath} saved to {output}")
+    except FileNotFoundError:
+      print(emoji.emojize(":cross_mark: "),f"No file found for {filepath}")
+
+
