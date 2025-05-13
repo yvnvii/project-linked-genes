f37be52535a1b44b3d56d01cc940d797602cb90b
yvnvii <yo2368@columbia.edu>
2025-05-13 10:22:41 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Call `pip install -e .` to install package locally for testing.
+"""
+
+from setuptools import setup
+
+# build command
+setup(
+    name="ldexplorer",
+    version="0.0.1",
+    author="Yuki Ogawa",
+    author_email="yo2368@columbia.edu",
+    license="GPLv3",
+    description="A package for calculating risks of carrying linked alleles only based on phenotypic traits",
+    classifiers=["Programming Language :: Python :: 3"],
+    entry_points={
+        "console_scripts": ["ldexplorer = ldexplorer.__main__:main"]
+    },
+)
