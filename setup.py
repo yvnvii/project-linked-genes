ef88f5bef6232eec3789d5b208a0d1b41c9fee76
yvnvii <yo2368@columbia.edu>
2025-05-13 10:32:07 -0400

    edit setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 Call `pip install -e .` to install package locally for testing.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # build command
 setup(
@@ -15,7 +15,10 @@ setup(
     license="GPLv3",
     description="A package for calculating risks of carrying linked alleles only based on phenotypic traits",
     classifiers=["Programming Language :: Python :: 3"],
+    packages=find_packages(where="final_submission", include=["ldexplorer", "ldexplorer.*"]),
+    package_dir={"": "final_submission"},
     entry_points={
         "console_scripts": ["ldexplorer = ldexplorer.__main__:main"]
     },
 )
+
